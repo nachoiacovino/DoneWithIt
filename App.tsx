52c5fca50a3aad07cac08c9a431d1624342bb107
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-09-20 17:15:51 +0200

    Create navigation theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, { useState } from 'react';
+import React from 'react';
 
 import AuthNavigator from './app/navigation/AuthNavigator';
+import navigationTheme from './app/navigation/navigationTheme';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navigationTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
